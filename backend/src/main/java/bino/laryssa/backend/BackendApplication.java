23f7f107122bdfb39b3f87d384e8a64ba4d9eb87
larybino <binolaryssa@gmail.com>
2026-09-19 15:15:38 -0300

    feat(backend): add timezone

--- backend/src/main/java/bino/laryssa/backend/BackendApplication.java
+++ backend/src/main/java/bino/laryssa/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package bino.laryssa.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
